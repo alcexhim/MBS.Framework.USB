7f75203341eff2e9ba069a2ad0d94f7f062ca057
Michael Becker <alcexhim@gmail.com>
2020-01-06 00:37:21 -0500

    add test project

---
+++ Applications/LibUSB.TestProject/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace LibUSB.TestProject
+{
+	class MainClass
+	{
+		public static void Main(string[] args)
+		{
+			System.Diagnostics.Process.Start("lsusb");
+
+			using (Context ctx = new Context())
+			{
+				while (true)
+				{
+					Console.WriteLine();
+					Console.Write("Enter Device ID (vid:pid) > ");
+					string vidpid = Console.ReadLine();
+					if (vidpid == "exit")
+					{
+						break;
+					}
+
+					if (!vidpid.Contains(":"))
+					{
+						Console.WriteLine("not in vid:pid format");
+						continue;
+					}
+
+					string[] vidpid_n = vidpid.Split(new char[] { ':' });
+					if (vidpid_n.Length != 2)
+					{
+						Console.WriteLine("not in vid:pid format");
+						continue;
+					}
+
+					string vid_s = vidpid_n[0];
+					string pid_s = vidpid_n[1];
+
+					ushort vid = UInt16.Parse(vid_s, System.Globalization.NumberStyles.HexNumber);
+					ushort pid = UInt16.Parse(pid_s, System.Globalization.NumberStyles.HexNumber);
+
+					Device[] devs = ctx.GetDevices(vid, pid);
+					Console.WriteLine("{0} devices found", devs.Length);
+				}
+			}
+		}
+	}
+}
